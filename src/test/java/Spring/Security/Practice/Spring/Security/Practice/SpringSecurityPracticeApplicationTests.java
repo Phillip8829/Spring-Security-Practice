ff108bf27ceeb957dacabe2feb0076f410e9e665
Phillip8829 <phillipyates12@hotmail.com>
2022-11-21 11:26:36 -0600

    START

---
+++ src/test/java/Spring/Security/Practice/Spring/Security/Practice/SpringSecurityPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring.Security.Practice.Spring.Security.Practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
